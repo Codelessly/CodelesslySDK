18ab47203dea21f6660079daa8adfc6cc5554c70
Saad Ardati <saadodi44@gmail.com>
2023-05-10 15:54:32 +0300

    Saad Asset Library #1

---
+++ lib/src/model/asset_model.dart
@@ -0,0 +1,76 @@
+import 'package:codelessly_api/codelessly_api.dart';
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'asset_model.g.dart';
+
+/// Defines an asset that is utilized throughout node data as an id and
+/// looked up for information about the asset based on that id.
+@JsonSerializable()
+class AssetModel with EquatableMixin {
+  /// The id of the asset.
+  final String id;
+
+  /// The user-facing label of the asset.
+  final String name;
+
+  /// The url of the asset.
+  final String url;
+
+  /// The source width of the asset.
+  final double sourceWidth;
+
+  /// The source height of the asset.
+  final double sourceHeight;
+
+  /// Date and time when the asset was created.
+  @JsonKey(fromJson: jsonToDate, toJson: dateToJson)
+  final DateTime createdAt;
+
+  /// Creates a new asset model.
+  AssetModel({
+    required this.id,
+    required this.name,
+    required this.url,
+    required this.sourceWidth,
+    required this.sourceHeight,
+    required this.createdAt,
+  });
+
+  /// Creates a copy of this asset model but with the given fields replaced
+  /// with the new values.
+  AssetModel copyWith({
+    String? id,
+    String? name,
+    String? url,
+    double? sourceWidth,
+    double? sourceHeight,
+    DateTime? createdAt,
+  }) {
+    return AssetModel(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      url: url ?? this.url,
+      sourceWidth: sourceWidth ?? this.sourceWidth,
+      sourceHeight: sourceHeight ?? this.sourceHeight,
+      createdAt: createdAt ?? this.createdAt,
+    );
+  }
+
+  /// Creates a new asset model from a json map.
+  factory AssetModel.fromJson(Map<String, dynamic> json) =>
+      _$AssetModelFromJson(json);
+
+  /// Converts this asset model to a json map.
+  Map<String, dynamic> toJson() => _$AssetModelToJson(this);
+
+  @override
+  List<Object?> get props => [
+        id,
+        name,
+        url,
+        sourceWidth,
+        sourceHeight,
+        createdAt,
+      ];
+}
---
+++ lib/src/model/asset_model.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'asset_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+AssetModel _$AssetModelFromJson(Map json) => AssetModel(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      url: json['url'] as String,
+      sourceWidth: (json['sourceWidth'] as num).toDouble(),
+      sourceHeight: (json['sourceHeight'] as num).toDouble(),
+      createdAt: jsonToDate(json['createdAt'] as int?),
+    );
+
+Map<String, dynamic> _$AssetModelToJson(AssetModel instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'url': instance.url,
+      'sourceWidth': instance.sourceWidth,
+      'sourceHeight': instance.sourceHeight,
+      'createdAt': dateToJson(instance.createdAt),
+    };
