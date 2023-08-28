05356ea7e0384c21de13005cf171b89a4b1c290a
birjuvachhani <brvachhani@gmail.com>
2023-08-28 14:23:48 +0530

    Add actions support for passive icon transformer.

--- lib/src/transformers/node_transformers/passive_icon_transformer.dart
+++ lib/src/transformers/node_transformers/passive_icon_transformer.dart
@@ -4,6 +4,7 @@ import 'package:codelessly_api/codelessly_api.dart';
 import 'package:flutter/material.dart';
 
 import '../../../codelessly_sdk.dart';
+import '../../functions/functions_repository.dart';
 
 const String kIconBaseUrl =
     'https://fonts.gstatic.com/s/i/materialiconsoutlined/home/v16/24px.svg';
@@ -17,38 +18,62 @@ class PassiveIconTransformer extends NodeWidgetTransformer<IconNode> {
     BuildContext context, [
     WidgetBuildSettings settings = const WidgetBuildSettings(),
   ]) {
-    return buildFromNode(node);
+    return buildFromNode(context, node);
   }
 
-  Widget buildFromNode(IconNode node) {
-    return PassiveIconWidget(node: node);
+  Widget buildFromNode(BuildContext context, IconNode node) {
+    return PassiveIconWidget(
+      node: node,
+      onPressed: () => onPressed(context, node),
+      onLongPressed: () => onLongPressed(context, node),
+    );
   }
+
+  void onPressed(BuildContext context, IconNode node) =>
+      FunctionsRepository.triggerAction(context, node, TriggerType.click);
+
+  void onLongPressed(BuildContext context, IconNode node) =>
+      FunctionsRepository.triggerAction(context, node, TriggerType.longPress);
 }
 
 class PassiveIconWidget extends StatelessWidget {
   final IconNode node;
   final bool useFonts;
+  final VoidCallback? onPressed;
+  final VoidCallback? onLongPressed;
 
   const PassiveIconWidget({
     super.key,
     required this.node,
     this.useFonts = false,
+    this.onPressed,
+    this.onLongPressed,
   });
 
   @override
   Widget build(BuildContext context) {
     final IconModel icon = node.icon;
 
-    return SvgIcon(
+    final iconWidget = SvgIcon(
       icon: icon,
       color: node.color?.toFlutterColor(),
       size: min(node.basicBoxLocal.width, node.basicBoxLocal.height),
     );
 
-    // return Icon(
+    // final iconWidget = Icon(
     //   useFonts ? icon.toFontIconData() : icon.toFlutterIconData(),
     //   size: min(node.basicBoxLocal.width, node.basicBoxLocal.height),
     //   color: node.color?.toFlutterColor(),
     // );
+
+    if (node.reactions.isNotEmpty) {
+      return GestureDetector(
+        onTap: onPressed,
+        onLongPress: onLongPressed,
+        child: iconWidget,
+      );
+    }
+
+    return iconWidget;
   }
 }
