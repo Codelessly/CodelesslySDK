eac899d6de27cf771f52eb32a4d14f544e2b1b0f
birjuvachhani <brvachhani@gmail.com>
2023-10-06 11:14:34 +0530

    Update codegen

--- lib/src/logging/codelessly_event.g.dart
+++ lib/src/logging/codelessly_event.g.dart
@@ -18,16 +18,66 @@ CodelesslyEvent _$CodelesslyEventFromJson(Map json) => CodelesslyEvent(
 Map<String, dynamic> _$CodelesslyEventToJson(CodelesslyEvent instance) {
   final val = <String, dynamic>{};
 
-  void writeNotNull(String key, dynamic value) {
-    if (value != null) {
-      val[key] = value;
+  /// Code from: https://github.com/google/quiver-dart/blob/master/lib/src/collection/utils.dart
+  bool listsEqual(List? a, List? b) {
+    if (a == b) return true;
+    if (a == null || b == null) return false;
+    if (a.length != b.length) return false;
+
+    for (int i = 0; i < a.length; i++) {
+      if (a[i] != b[i]) return false;
+    }
+
+    return true;
+  }
+
+  /// Code from: https://github.com/google/quiver-dart/blob/master/lib/src/collection/utils.dart
+  bool mapsEqual(Map? a, Map? b) {
+    if (a == b) return true;
+    if (a == null || b == null) return false;
+    if (a.length != b.length) return false;
+
+    for (final k in a.keys) {
+      var bValue = b[k];
+      if (bValue == null && !b.containsKey(k)) return false;
+      if (bValue != a[k]) return false;
+    }
+
+    return true;
+  }
+
+  /// Code from: https://github.com/google/quiver-dart/blob/master/lib/src/collection/utils.dart
+  bool setsEqual(Set? a, Set? b) {
+    if (a == b) return true;
+    if (a == null || b == null) return false;
+    if (a.length != b.length) return false;
+
+    return a.containsAll(b);
+  }
+
+  void writeNotNull(
+      String key, dynamic value, dynamic jsonValue, dynamic defaultValue) {
+    if (value == null) return;
+    bool areEqual = false;
+    if (value is List) {
+      areEqual = listsEqual(value, defaultValue);
+    } else if (value is Map) {
+      areEqual = mapsEqual(value, defaultValue);
+    } else if (value is Set) {
+      areEqual = setsEqual(value, defaultValue);
+    } else {
+      areEqual = value == defaultValue;
+    }
+
+    if (!areEqual) {
+      val[key] = jsonValue;
     }
   }
 
-  writeNotNull('message', instance.message);
-  writeNotNull(
-      'timestamp', const DateTimeConverter().toJson(instance.timestamp));
-  writeNotNull('stacktrace', instance.stacktrace);
-  val['tags'] = instance.tags;
+  writeNotNull('message', instance.message, instance.message, null);
+  writeNotNull('timestamp', instance.timestamp,
+      const DateTimeConverter().toJson(instance.timestamp), null);
+  writeNotNull('stacktrace', instance.stacktrace, instance.stacktrace, null);
+  writeNotNull('tags', instance.tags, instance.tags, const []);
   return val;
 }
--- lib/src/model/asset_model.g.dart
+++ lib/src/model/asset_model.g.dart
@@ -25,14 +25,64 @@ Map<String, dynamic> _$AssetModelToJson(AssetModel instance) {
     'sourceHeight': instance.sourceHeight,
   };
 
-  void writeNotNull(String key, dynamic value) {
-    if (value != null) {
-      val[key] = value;
+  /// Code from: https://github.com/google/quiver-dart/blob/master/lib/src/collection/utils.dart
+  bool listsEqual(List? a, List? b) {
+    if (a == b) return true;
+    if (a == null || b == null) return false;
+    if (a.length != b.length) return false;
+
+    for (int i = 0; i < a.length; i++) {
+      if (a[i] != b[i]) return false;
+    }
+
+    return true;
+  }
+
+  /// Code from: https://github.com/google/quiver-dart/blob/master/lib/src/collection/utils.dart
+  bool mapsEqual(Map? a, Map? b) {
+    if (a == b) return true;
+    if (a == null || b == null) return false;
+    if (a.length != b.length) return false;
+
+    for (final k in a.keys) {
+      var bValue = b[k];
+      if (bValue == null && !b.containsKey(k)) return false;
+      if (bValue != a[k]) return false;
+    }
+
+    return true;
+  }
+
+  /// Code from: https://github.com/google/quiver-dart/blob/master/lib/src/collection/utils.dart
+  bool setsEqual(Set? a, Set? b) {
+    if (a == b) return true;
+    if (a == null || b == null) return false;
+    if (a.length != b.length) return false;
+
+    return a.containsAll(b);
+  }
+
+  void writeNotNull(
+      String key, dynamic value, dynamic jsonValue, dynamic defaultValue) {
+    if (value == null) return;
+    bool areEqual = false;
+    if (value is List) {
+      areEqual = listsEqual(value, defaultValue);
+    } else if (value is Map) {
+      areEqual = mapsEqual(value, defaultValue);
+    } else if (value is Set) {
+      areEqual = setsEqual(value, defaultValue);
+    } else {
+      areEqual = value == defaultValue;
+    }
+
+    if (!areEqual) {
+      val[key] = jsonValue;
     }
   }
 
-  writeNotNull(
-      'createdAt', const DateTimeConverter().toJson(instance.createdAt));
-  val['blurHash'] = instance.blurHash;
+  writeNotNull('createdAt', instance.createdAt,
+      const DateTimeConverter().toJson(instance.createdAt), null);
+  writeNotNull('blurHash', instance.blurHash, instance.blurHash, '');
   return val;
 }
