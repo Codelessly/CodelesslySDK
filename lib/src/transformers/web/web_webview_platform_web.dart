0c20c613eaa397186cfb6e1e5bec91ae5047d6c7
Saad Ardati <saadodi44@gmail.com>
2023-10-26 19:27:14 +0300

    Admin Dashboard!

--- lib/src/transformers/web/web_webview_platform_web.dart
+++ lib/src/transformers/web/web_webview_platform_web.dart
@@ -1,5 +1,6 @@
 /// This file is used to import the [WebWebViewPlatform] class from the
 /// `webview_flutter_web` package. This is done to avoid errors on non-web
 /// platforms.
+library;
 
 export 'package:webview_flutter_web/webview_flutter_web.dart';
