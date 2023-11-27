f28651cc56736399a8a845aa1e7cf6a3dbd800e0
birjuvachhani <brvachhani@gmail.com>
2023-11-27 11:59:36 +0530

    SDK Cloud Storage #10
    - Add code docs.

--- lib/src/data/cloud_storage.dart
+++ lib/src/data/cloud_storage.dart
@@ -6,12 +6,59 @@ import 'package:flutter/cupertino.dart';
 
 import '../../codelessly_sdk.dart';
 
+/// Allows access to cloud storage. Implementations of this class should be
+/// able to store and retrieve data from the cloud storage in secure manner.
+/// This is mainly used to store data on cloud via actions for the SDK.
+///
+/// This abstraction provides access to the cloud storage that is NoSQL in
+/// nature. The storage is organized in a tree like structure.
+///
+/// Each node in the tree is a document. Each document can have a list of
+/// sub-Collections and each sub-Collection can have a list of documents.
+///
+/// Each document can have a list of key-value pairs. The key is a string and
+/// the value is an object. The object can be any primitive type or a list or
+/// map of primitive types depending on the implementation.
+///
+/// [identifier] is used to identify the instance of the cloud storage this
+/// SDK session is using. This is used to separate data for different projects
+/// and users. The actual identity of this [identifier] depends on the
+/// implementation.
 abstract class CloudStorage extends ChangeNotifier {
+  /// Used to identify the instance of the cloud storage this SDK session is
+  /// using. This is used to separate data for different projects and users.
+  /// The actual identity of this depends on the implementation.
+  ///
+  /// This cannot be an empty string.
   final String identifier;
 
+  /// Creates a new instance of [CloudStorage] with the given [identifier].
+  /// The [identifier] cannot be empty. The actual identity of this depends on
+  /// the implementation.
+  ///
+  /// It will throw an [AssertionError] if the [identifier] is empty.
   CloudStorage(this.identifier)
       : assert(identifier.isNotEmpty, 'identifier cannot be empty');
 
+  /// Adds a document to the cloud storage at the given [path].
+  ///
+  /// The [path] is a string that represents the path to the document
+  /// excluding the document id. The path must always resolve to a collection.
+  /// If the [path] is empty, then the document is added to "default"
+  /// collection. If the path ends in a document path, then "default" is
+  /// suffixed to the path to make it a collection path.
+  ///
+  /// If the document does not exist, then it is created. If the document
+  /// already exists, then we skip the creation of the document and return
+  /// true if [skipCreationIfDocumentExists] is true. Otherwise it will be
+  /// overwritten with given [value].
+  ///
+  /// If [autoGenerateId] is true, then the document id is auto generated and
+  /// [documentId] is ignored. If [autoGenerateId] is false, then the document
+  /// id is [documentId]. If [documentId] is null or empty, then "default" is
+  /// used as the document id.
+  ///
+  /// [value] is the data to be stored in the document.
   Future<bool> addDocument(
     String path, {
     String? documentId,
@@ -20,18 +67,88 @@ abstract class CloudStorage extends ChangeNotifier {
     required Map<String, dynamic> value,
   });
 
+  /// Updates a document in the cloud storage at the given [path] with the
+  /// given [documentId].
+  ///
+  /// The [path] is a string that represents the path to the document
+  /// excluding the document id. The path must always resolve to a collection.
+  /// If the [path] is empty, then the document is added to "default"
+  /// collection. If the path ends in a document path, then "default" is
+  /// suffixed to the path to make it a collection path.
+  ///
+  /// [documentId] is the id of the document. If [documentId] is empty, then
+  /// "default" is used as the document id.
+  ///
+  /// If the document does not exist, then it is created. If the document
+  /// already exists, then we merge given [value] with existing document.
   Future<bool> updateDocument(
     String path, {
     required String documentId,
     required Map<String, dynamic> value,
   });
 
+  /// Removes a document from the cloud storage at the given [path] with the
+  /// given [documentId].
+  ///
+  /// The [path] is a string that represents the path to the document
+  /// excluding the document id. The path must always resolve to a collection.
+  /// If the [path] is empty, then the document is added to "default"
+  /// collection. If the path ends in a document path, then "default" is
+  /// suffixed to the path to make it a collection path.
+  ///
+  /// Path can be separated by '/' or '.'.
+  ///
+  /// [documentId] is the id of the document. If [documentId] is empty, then
+  /// "default" is used as the document id.
+  ///
+  /// If the document does not exist, then it will return false. Otherwise
+  /// it will return true.
   Future<bool> removeDocument(String path, String documentId);
 
+  /// Retrieves the document data from the cloud storage at the given [path]
+  /// with the given [documentId].
+  ///
+  /// The [path] is a string that represents the path to the document
+  /// excluding the document id. The path must always resolve to a collection.
+  /// If the [path] is empty, then the document is added to "default"
+  /// collection. If the path ends in a document path, then "default" is
+  /// suffixed to the path to make it a collection path.
+  ///
+  /// Path can be separated by '/' or '.'.
+  ///
+  /// [documentId] is the id of the document. If [documentId] is empty, then
+  /// "default" is used as the document id.
   Future<Map<String, dynamic>> getDocumentData(String path, String documentId);
 
+  /// Streams document data from the cloud storage at the given [path]
+  /// with the given [documentId].
+  ///
+  /// The [path] is a string that represents the path to the document
+  /// excluding the document id. The path must always resolve to a collection.
+  /// If the [path] is empty, then the document is added to "default"
+  /// collection. If the path ends in a document path, then "default" is
+  /// suffixed to the path to make it a collection path.
+  ///
+  /// Path can be separated by '/' or '.'.
+  ///
+  /// [documentId] is the id of the document. If [documentId] is empty, then
+  /// "default" is used as the document id.
   Stream<Map<String, dynamic>> streamDocument(String path, String documentId);
 
+  /// Streams document data from the cloud storage at the given [path]
+  /// with the given [documentId] and updates the given [variable] with the
+  /// data when there is an update.
+  ///
+  /// The [path] is a string that represents the path to the document
+  /// excluding the document id. The path must always resolve to a collection.
+  /// If the [path] is empty, then the document is added to "default"
+  /// collection. If the path ends in a document path, then "default" is
+  /// suffixed to the path to make it a collection path.
+  ///
+  /// Path can be separated by '/' or '.'.
+  ///
+  /// [documentId] is the id of the document. If [documentId] is empty, then
+  /// "default" is used as the document id.
   void streamDocumentToVariable(
     String path,
     String documentId,
@@ -39,54 +156,96 @@ abstract class CloudStorage extends ChangeNotifier {
   );
 }
 
+/// A [CloudStorage] implementation that uses Firestore as the backend.
+/// This is used to store data via actions for the SDK.
 class FirestoreCloudStorage extends CloudStorage {
+  /// Reference to the root document for this session.
   late final DocumentReference<Map<String, dynamic>> rootRef =
       firestore.collection('data').doc(identifier);
 
+  /// Reference to the Firestore instance.
   final FirebaseFirestore firestore;
 
+  /// Subscriptions to the streams that are being listened to.
   final List<StreamSubscription> _subscriptions = [];
 
+  /// Creates a [FirestoreCloudStorage] with the given [identifier] and
+  /// [firestore] instance.
   FirestoreCloudStorage(super.identifier, this.firestore);
 
+  /// Initializes the cloud storage for the given [identifier]. This must be
+  /// called and awaited before using the cloud storage.
   Future<void> init() async {
     print(
         'Initializing FirestoreCloudStorage for $identifier at ${rootRef.path}');
     // Create project doc if missing.
     final snapshot = await rootRef.get();
+
+    // Do nothing if project doc exists.
     if (snapshot.exists) return;
+
+    // Create project doc if it does not exist.
     await rootRef.set({'project': identifier});
   }
 
-  // Gives a collection reference for the given path.
+  /// Gives a collection reference for the given [path].
+  ///
+  /// The [path] is a string that represents the path to the document
+  /// excluding the document id. The path must always resolve to a collection.
+  /// If the [path] is empty, then the document is added to "default"
+  /// collection. If the path ends in a document path, then "default" is
+  /// suffixed to the path to make it a collection path.
+  ///
+  /// Path can be separated by '/' or '.'.
   CollectionReference<Map<String, dynamic>> getCollectionPath(String path) {
-    if (path.trim().isNotEmpty) {
-      // path is provided.
-      final pathParts = path.split(pathSeparatorRegex);
-      CollectionReference<Map<String, dynamic>>? ref;
-      while (pathParts.isNotEmpty) {
-        final part = pathParts.removeAt(0);
-        if (ref == null) {
-          ref = rootRef.collection(part);
-        } else {
-          final String id =
-              pathParts.isNotEmpty ? pathParts.removeAt(0) : 'default';
-          ref = ref.doc(part).collection(id);
-        }
+    path = path.trim();
+
+    // Return default collection if path is empty.
+    if (path.isEmpty) return rootRef.collection('default');
+
+    // Split path by '/' or '.'.
+    final pathParts = path.split(pathSeparatorRegex);
+
+    // Construct collection reference by iterating over path parts.
+    CollectionReference<Map<String, dynamic>>? ref;
+    while (pathParts.isNotEmpty) {
+      // Get the first part of the remaining path.
+      final part = pathParts.removeAt(0);
+      if (ref == null) {
+        // Initialize ref with a collection reference to the first part of the
+        // path if ref is null.
+        ref = rootRef.collection(part);
+      } else {
+        // this means the ref represents a collection, so we add a collection
+        // with the first part of the path as the id.
+        final String id =
+            pathParts.isNotEmpty ? pathParts.removeAt(0) : 'default';
+        ref = ref.doc(part).collection(id);
       }
-      return ref!;
-    } else {
-      return rootRef.collection('default');
     }
+    return ref!;
   }
 
-  /// Gives a document reference for the given path. If docId is null, then
-  /// 'default' is used as the document id.
+  /// Gives a document reference for the given path.
+  ///
+  /// The [path] is a string that represents the path to the document
+  /// excluding the document id. The path must always resolve to a collection.
+  /// If the [path] is empty, then the document is added to "default"
+  /// collection. If the path ends in a document path, then "default" is
+  /// suffixed to the path to make it a collection path.
+  ///
+  /// Path can be separated by '/' or '.'.
+  ///
+  /// [documentId] is the id of the document. If [documentId] is null or empty,
+  /// then "default" is used as the document id.
   DocumentReference<Map<String, dynamic>> getDocPath(
-      String path, String? docId) {
-    docId ??= 'default';
-    if (docId.trim().isEmpty) docId = 'default';
-    return getCollectionPath(path).doc(docId);
+      String path, String? documentId) {
+    // Use default if null.
+    documentId ??= 'default';
+
+    // Use default if empty.
+    if (documentId.trim().isEmpty) documentId = 'default';
+    return getCollectionPath(path).doc(documentId);
   }
 
   @override
@@ -103,16 +262,19 @@ class FirestoreCloudStorage extends CloudStorage {
       print('Document added: ${document.path}');
       return true;
     }
+    // Get doc reference.
     final DocumentReference docRef = getDocPath(path, documentId);
+
+    // Get snapshot to check if document exists.
     final snapshot = await docRef.get();
     if (skipCreationIfDocumentExists && snapshot.exists) {
       // if skipCreationIfDocumentExists is true, check if document exists.
       // if document exists, then return.
-      // TODO: should we update doc in this case?
       print('Document already exists: ${docRef.path}');
       return true;
     }
 
+    // Set document.
     await docRef.set(value);
     print('Document added: ${docRef.path}/$documentId');
     return true;
@@ -167,12 +329,18 @@ class FirestoreCloudStorage extends CloudStorage {
     String documentId,
     Observable<VariableData> variable,
   ) {
+    // Get doc reference.
     final docRef = getDocPath(path, documentId);
+
+    // Stream document.
     final stream = docRef.snapshots().map((snapshot) => snapshot.data() ?? {});
+
+    // Listen to the stream and update the variable.
     final subscription = stream.listen(
       (data) {
         print('Document stream update from cloud storage: $path/$documentId');
         print('Updating variable ${variable.value.name} with success state.');
+        // Set the variable with success state.
         variable.set(
           variable.value
               .copyWith(value: CloudStorageVariableUtils.success(data)),
@@ -180,6 +348,7 @@ class FirestoreCloudStorage extends CloudStorage {
       },
       onError: (error) {
         print('Error loading document from cloud storage: $path/$documentId');
+        // Set the variable with error state.
         variable.set(
           variable.value.copyWith(
             value: CloudStorageVariableUtils.error(error.toString()),
@@ -187,11 +356,14 @@ class FirestoreCloudStorage extends CloudStorage {
         );
       },
     );
+
+    // Add subscription to the list of subscriptions.
     _subscriptions.add(subscription);
   }
 
   @override
   void dispose() {
+    // Cancel all subscriptions.
     _subscriptions.forEach((sub) => sub.cancel());
     super.dispose();
   }
--- lib/src/data/local_storage.dart
+++ lib/src/data/local_storage.dart
@@ -4,50 +4,102 @@ import 'dart:developer';
 import 'package:flutter/cupertino.dart';
 import 'package:hive/hive.dart';
 
+/// Allows access to local storage on the device. Implementations of this class
+/// should be able to store and retrieve data from the device's local storage.
+/// This is mainly used to store data via actions for the SDK.
+///
+/// This abstraction provides access to the local storage via a key-value store.
+/// The key is a string and the value is an object. The object can be any
+/// primitive type or a list or map of primitive types depending on the
+/// implementation.
 abstract class LocalStorage extends ChangeNotifier {
-  final Map<String, StorageListenable> _notifiers = {};
+  /// A map of notifiers for a key in the storage. This is used to notify
+  /// listeners when the value for a key in the storage changes.
+  final Map<String, _StorageListenable> _notifiers = {};
 
+  /// Whether or not the storage contains the given [key].
   bool containsKey(String key);
 
+  /// Returns all the data in the storage as a map.
   Map<String, dynamic> getAll();
 
+  /// Stores the given [value] in the storage with the given [key].
   Future<void> put(String key, Object? value);
 
+  /// Returns the value stored in the storage with the given [key].
+  /// If the key is not found or has a null value, then
+  /// the [defaultValue] is returned.
   Object? get(String key, {Object? defaultValue});
 
+  /// Removes the value stored in the storage with the given [key].
   Future<void> remove(String key);
 
-  Future<void> close();
-
+  /// Clears all the data in the storage.
   Future<void> clear();
 
+  /// Returns a [_StorageListenable] that notifies when the value for the
+  /// given [key] in the storage changes. These listeners are automatically
+  /// disposed when the storage is closed using [close] method.
+  ///
+  /// If the [key] is null, then the returned listenable notifies when any
+  /// value in the storage changes.
   Listenable getNotifier(String? key);
+
+  @override
+  void dispose() {
+    // Dispose all the notifiers.
+    _notifiers.values.forEach((notifier) => notifier.dispose());
+    _notifiers.clear();
+
+    super.dispose();
+  }
 }
 
+/// A [LocalStorage] implementation that uses [Hive] as the underlying storage.
+/// This is used to store data via actions for the SDK.
 class HiveLocalStorage extends LocalStorage {
   final Box _box;
 
   late final StreamSubscription? _subscription;
 
+  /// Creates a [HiveLocalStorage] with the given [box].
   HiveLocalStorage(this._box) {
-    _subscription = _box.watch().listen(onBoxChanged);
+    // Listen to changes in the storage.
+    _subscription = _box.watch().listen(_onBoxChanged);
   }
 
-  void onBoxChanged(BoxEvent event) {
+  /// Called when the storage changes. This notifies the listeners for the
+  /// given [key] in the storage. This also notifies the listeners for the
+  /// storage itself.
+  void _onBoxChanged(BoxEvent event) {
     log('storage changed for key: ${event.key}');
+
+    // Get the notifier for the given key.
     final notifier = _notifiers[event.key.toString()];
+
+    // Notify the notifier for the given key if it exists.
     if (notifier != null) notifier.notify();
 
+    // Notify the notifier for the storage itself.
     notifyListeners();
   }
 
   @override
   Listenable getNotifier(String? key) {
+    // Return this as the listenable if key is null, notifying when any key
+    // in the storage changes.
     if (key == null) return this;
+
+    // Return the notifier for the given key if it already exists.
     if (_notifiers.containsKey(key)) return _notifiers[key]!;
 
-    final notifier = StorageListenable._(key);
+    // Create a new notifier for the given key.
+    final notifier = _StorageListenable._(key);
+
+    // Add the notifier to the map of notifiers.
     _notifiers[key] = notifier;
+
+    // Return the notifier.
     return notifier;
   }
 
@@ -62,6 +114,7 @@ class HiveLocalStorage extends LocalStorage {
 
   @override
   Object? get(String key, {Object? defaultValue}) {
+    // Return default value if key is not found.
     if (!_box.containsKey(key)) return defaultValue;
     return _box.get(key) ?? defaultValue;
   }
@@ -70,22 +123,29 @@ class HiveLocalStorage extends LocalStorage {
   Future<void> remove(String key) => _box.delete(key);
 
   @override
-  Future<void> close() {
+  void dispose() async {
+    // Cancel the subscription to the storage updates.
     _subscription?.cancel();
-    _notifiers.values.forEach((notifier) => notifier.dispose());
-    _notifiers.clear();
-    return _box.close();
+
+    // Close the box itself.
+    await _box.close();
+
+    super.dispose();
   }
 
   @override
   Future<void> clear() => _box.clear();
 }
 
-class StorageListenable extends ChangeNotifier {
+/// A [Listenable] that notifies when the value for the given [key] in the
+/// storage changes.
+class _StorageListenable extends ChangeNotifier {
   final String? _key;
 
-  StorageListenable._(this._key);
+  _StorageListenable._(this._key);
 
+  /// Notifies the listeners that the value for the given [key] in the storage
+  /// has changed.
   void notify() {
     log('notifying storage changed for key: $_key');
     notifyListeners();
