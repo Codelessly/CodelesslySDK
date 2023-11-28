06084fb448db552f84f66776552378379ec40858
birjuvachhani <brvachhani@gmail.com>
2023-11-28 10:37:22 +0530

    Move Firedart out of SDK.

--- lib/firedart.dart
+++
@@ -1,9 +0,0 @@
-library firedart;
-
-export 'src/firedart/auth/client.dart';
-export 'src/firedart/auth/exceptions.dart';
-export 'src/firedart/auth/firebase_auth.dart';
-export 'src/firedart/auth/token_store.dart';
-export 'src/firedart/auth/user_gateway.dart';
-export 'src/firedart/firestore/firestore.dart';
-export 'src/firedart/firestore/models.dart';
--- lib/src/firedart/platform/access_exporter.dart
+++
@@ -1,3 +0,0 @@
-export 'unsupported_access.dart'
-    if (dart.library.io) 'io_access.dart'
-    if (dart.library.html) 'web_access.dart';
