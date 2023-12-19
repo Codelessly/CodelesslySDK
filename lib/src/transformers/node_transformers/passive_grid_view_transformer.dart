74245f89bdb8bf6c8b669055fae1b7509a5a671e
birjuvachhani <brvachhani@gmail.com>
2023-12-19 14:04:23 +0530

    Add firebase queries support for grid view.

--- lib/src/transformers/node_transformers/passive_grid_view_transformer.dart
+++ lib/src/transformers/node_transformers/passive_grid_view_transformer.dart
@@ -1,7 +1,10 @@
 import 'dart:core';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:codelessly_api/codelessly_api.dart';
+import 'package:firebase_ui_firestore/firebase_ui_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../../codelessly_sdk.dart';
 
@@ -31,12 +34,115 @@ class PassiveGridViewWidget extends StatelessWidget {
     required this.settings,
   });
 
+  Query<Map<String, dynamic>> constructQuery(
+      BuildContext context, Codelessly codelessly) {
+    final PublishSource source =
+        codelessly.config?.publishSource ?? PublishSource.preview;
+
+    final String collectionPath = node.collectionPath ?? '';
+
+    Query<Map<String, dynamic>> query = codelessly.firebaseFirestore.collection(
+      '${source.rootDataCollection}/${codelessly.authManager.authData!.projectId}/$collectionPath',
+    );
+
+    return constructQueryFromRef(
+      query,
+      whereFilters: node.whereFilters,
+      orderByOperations: node.orderByFilters,
+      scopedValues: ScopedValues.of(context),
+      nullSubstitutionMode: settings.nullSubstitutionMode,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     if (node.children.isEmpty) {
       return AdaptiveNodeBox(node: node, child: const SizedBox());
     }
     final itemNode = node.children.first;
+
+    final bool useCloudDatabase = node.useCloudDatabase;
+    if (useCloudDatabase && !settings.isPreview) {
+      final codelessly = context.read<Codelessly>();
+      final codelesslyController = context.read<CodelesslyWidgetController>();
+
+      if (codelessly.authManager.authData == null) {
+        return codelesslyController.loadingBuilder?.call(context) ??
+            const Center(child: CircularProgressIndicator());
+      }
+
+      final query = constructQuery(context, codelessly);
+
+      return AdaptiveNodeBox(
+        node: node,
+        child: FirestoreQueryBuilder<Map<String, dynamic>>(
+          query: query,
+          pageSize: node.limit ?? 20,
+          builder: (context, snapshot, child) {
+            if (snapshot.isFetching) {
+              return codelesslyController.loadingBuilder?.call(context) ??
+                  const Center(child: CircularProgressIndicator());
+            }
+
+            if (snapshot.hasError) {
+              return codelesslyController.errorBuilder?.call(
+                    context,
+                    snapshot.error,
+                  ) ??
+                  const Center(child: Text('Error'));
+            }
+
+            final List<QueryDocumentSnapshot<Map>> docs = snapshot.docs;
+            final itemCount = docs.length;
+
+            return GridViewBuilder(
+              primary: node.primary,
+              gridDelegate: node.properties.gridDelegate,
+              shrinkWrap: node.properties.itemCount != null &&
+                  (node.scrollDirection == AxisC.horizontal
+                      ? node.isHorizontalWrap
+                      : node.isVerticalWrap),
+              itemCount: itemCount,
+              padding: node.padding.flutterEdgeInsets,
+              keyboardDismissBehavior:
+                  node.keyboardDismissBehavior.flutterKeyboardDismissBehavior,
+              physics:
+                  node.physics.flutterScrollPhysics(node.shouldAlwaysScroll),
+              scrollDirection: node.scrollDirection.flutterAxis,
+              cacheExtent: node.properties.cacheExtent,
+              reverse: node.reverse,
+              clipBehavior: node.clipsContent ? Clip.hardEdge : Clip.none,
+              itemBuilder: (context, index) {
+                final doc = docs.elementAtOrNull(index);
+                final id = doc?.id;
+                final data = doc?.data();
+                return IndexedItemProvider(
+                  key: ValueKey(index),
+                  item: IndexedItem(
+                    index,
+                    {if (id != null) 'id': id, ...?data},
+                  ),
+                  child: Builder(
+                    builder: (context) {
+                      // This builder is important to pass a context that has
+                      // the IndexedItemProvider.
+                      return KeyedSubtree(
+                        key: ValueKey(index),
+                        child: manager.buildWidgetByID(
+                          itemNode,
+                          context,
+                          settings: settings,
+                        ),
+                      );
+                    },
+                  ),
+                );
+              },
+            );
+          },
+        ),
+      );
+    }
 
     final List? data = PropertyValueDelegate.getVariableValueFromPath<List>(
           node.variables['data'] ?? '',
@@ -72,18 +178,20 @@ class PassiveGridViewWidget extends StatelessWidget {
         itemBuilder: (context, index) => IndexedItemProvider(
           key: ValueKey(index),
           item: IndexedItem(index, data?.elementAtOrNull(index)),
-          child: Builder(builder: (context) {
-            // This builder is important to pass a context that has
-            // the IndexedItemProvider.
-            return KeyedSubtree(
-              key: ValueKey(index),
-              child: manager.buildWidgetByID(
-                itemNode,
-                context,
-                settings: settings,
-              ),
-            );
-          }),
+          child: Builder(
+            builder: (context) {
+              // This builder is important to pass a context that has
+              // the IndexedItemProvider.
+              return KeyedSubtree(
+                key: ValueKey(index),
+                child: manager.buildWidgetByID(
+                  itemNode,
+                  context,
+                  settings: settings,
+                ),
+              );
+            },
+          ),
         ),
       ),
     );
