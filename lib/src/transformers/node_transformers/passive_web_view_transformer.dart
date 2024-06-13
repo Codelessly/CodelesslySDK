4ce478d9718cdb61b62a1072083c922972c784c7
Saad Ardati <saadodi44@gmail.com>
2024-06-13 17:14:25 +0300

    Disable setNavigationDelegate in passive_web_view_transformer.dart

--- lib/src/transformers/node_transformers/passive_web_view_transformer.dart
+++ lib/src/transformers/node_transformers/passive_web_view_transformer.dart
@@ -208,10 +208,11 @@ class _RawWebViewWidgetState extends State<RawWebViewWidget> {
       _controller.setBackgroundColor(
           props.backgroundColor?.toFlutterColor() ?? Colors.transparent);
     }
-    _controller.setNavigationDelegate(NavigationDelegate(
-      onPageFinished: (url) => widget.onPageLoaded?.call(_controller, url),
-      onPageStarted: (url) => widget.onPageStarted?.call(_controller, url),
-    ));
+
+    // _controller.setNavigationDelegate(NavigationDelegate(
+    //   onPageFinished: (url) => widget.onPageLoaded?.call(_controller, url),
+    //   onPageStarted: (url) => widget.onPageStarted?.call(_controller, url),
+    // ));
   }
 
   Future<void> _loadData() {
