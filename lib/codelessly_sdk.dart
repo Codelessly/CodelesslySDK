b0622ce16db7ee345a47bc2ec8384198ea3d0db9
birjuvachhani <brvachhani@gmail.com>
2024-07-01 12:47:51 +0530

    export CodelesslyException from the SDK.

--- lib/codelessly_sdk.dart
+++ lib/codelessly_sdk.dart
@@ -16,6 +16,7 @@ export 'src/data/network_data_repository.dart';
 export 'src/data/web_data_repository.dart';
 export 'src/functions/functions.dart';
 export 'src/logging/codelessly_logger.dart';
+export 'src/logging/error_handler.dart';
 export 'src/model/asset_model.dart';
 export 'src/model/auth_data.dart';
 export 'src/model/custom_component.dart';
